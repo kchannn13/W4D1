d4a452f287993b029f228f0aa657e943ef2c03bf
Kevin Chan <chankevin13@gmail.com>
2023-01-30 11:38:17 -0500

    valid_moves

--- knights_travails.rb
+++ knights_travails.rb
@@ -10,9 +10,32 @@ class KnightPathFinder
         
         valid_pos = []
 
-        pos.each do |x,y|
+    #     pos.each_index do |i|
+    #         if i == 0 && pos[i] < 7
+    #             x = pos[i] + 2
+    #         elsif i == 1 && pos[i] < 8
+    #             y = pos[i] + 1
+    #         end
+    #         valid_pos << [x,y]
+    #     end
+    #     valid_pos
+    # end
 
+
+        x, y = pos
+
+        if x < 6 && y < 7
+            x += 2
+            y += 1
         end
+        valid_pos << [x,y]
+        return valid_pos
     end
+
+
+
+
+
+
 
 end
