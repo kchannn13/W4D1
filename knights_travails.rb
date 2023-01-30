8ece0762ca067a28ae8ae8082b2a7cb890a86cbf
Kevin Chan <chankevin13@gmail.com>
2023-01-30 11:38:52 -0500

    deleted

--- knights_travails.rb
+++ knights_travails.rb
@@ -5,22 +5,8 @@ class KnightPathFinder
     end
 
 
-    def self.valid_moves(pos)
-        # x, y = pos[0], pos[1]
-        
+    def self.valid_moves(pos)     
         valid_pos = []
-
-    #     pos.each_index do |i|
-    #         if i == 0 && pos[i] < 7
-    #             x = pos[i] + 2
-    #         elsif i == 1 && pos[i] < 8
-    #             y = pos[i] + 1
-    #         end
-    #         valid_pos << [x,y]
-    #     end
-    #     valid_pos
-    # end
-
 
         x, y = pos
 
