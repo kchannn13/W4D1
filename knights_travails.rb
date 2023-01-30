6f13588948c058553a95d3ad8c09697b49bf041f
Charles Cruse <charle9@vt.edu>
2023-01-30 11:18:51 -0500

    started valid moves

---
+++ knights_travails.rb
@@ -0,0 +1,18 @@
+class KnightPathFinder
+
+    def initialize(position)
+        @position = position
+    end
+
+
+    def self.valid_moves(pos)
+        # x, y = pos[0], pos[1]
+        
+        valid_pos = []
+
+        pos.each do |x,y|
+
+        end
+    end
+
+end
